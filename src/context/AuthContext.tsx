792ae3b02b03de30ab092a3b04a0c158c095257f
piyush0049 <tanmayv2504@gmail.com>
2024-08-26 16:38:19 +0530

    first commit

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,8 @@
 import React, { createContext, useState, ReactNode, useContext, useCallback, useEffect } from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
 
 interface AppContextType {
+  // Authentication related
   isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
   isAdmin: string;
@@ -10,6 +10,8 @@ interface AppContextType {
   loading: boolean;
   setloading: React.Dispatch<React.SetStateAction<boolean>>;
   refreshAuth: () => void;
+
+  // Search related
   searchQuery: string;
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 }
@@ -17,20 +19,21 @@ interface AppContextType {
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  // Authentication states
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [isAdmin, setIsAdmin] = useState<string>("user");
   const [loading, setloading] = useState<boolean>(true);
+
+  // Search states
   const [searchQuery, setSearchQuery] = useState<string>("");
 
   const PORT = "https://603-cws-backend.vercel.app";
-
-  const location = useLocation();
 
   const checkAuth = useCallback(async () => {
     try {
       const res = await axios.get(`${PORT}/api/v1/users/checkauth`, { withCredentials: true });
       setIsAuthenticated(res.data.auth);
-      setIsAdmin(res.data.user);
+      setIsAdmin(res.data.user); // Assuming res.data.user contains the role
     } catch (error) {
       console.error("Error checking authentication:", error);
       setIsAuthenticated(false);
@@ -40,14 +43,14 @@ export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => 
     }
   }, [PORT]);
 
-  const refreshAuth = useCallback(() => {
+  const refreshAuth = () => {
     setloading(true);
     checkAuth();
-  }, [checkAuth]);
+  };
 
   useEffect(() => {
-    refreshAuth();
-  }, [location.pathname, refreshAuth]);
+    checkAuth();
+  }, [checkAuth]);
 
   return (
     <AppContext.Provider value={{ 
