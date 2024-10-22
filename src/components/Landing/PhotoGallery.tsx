ac1d82d52033d229d9c941688412376443eadbbe
piyush0049 <piyushjoshi81204@gmail.com>
2024-10-22 18:49:00 +0530

    changes

--- src/components/Landing/PhotoGallery.tsx
+++ src/components/Landing/PhotoGallery.tsx
@@ -3,13 +3,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import './../../../node_modules/swiper/swiper-bundle.min.css';
 import SwiperCore from 'swiper';
 import { Autoplay } from 'swiper/modules';
-import { gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9 } from '../../utils/Landing/Landing';
+import { gallery1, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9 } from '../../utils/Landing/Landing';
 
 SwiperCore.use([Autoplay]);
 
 const photos = [
   { src: `${gallery1}`, alt: 'Photo 1' },
-  { src: `${gallery2}`, alt: 'Photo 2' },
   { src: `${gallery3}`, alt: 'Photo 3' },
   { src: `${gallery4}`, alt: 'Photo 4' },
   { src: `${gallery5}`, alt: 'Photo 5' },
@@ -21,8 +20,8 @@ const photos = [
 
 const PhotoGallery: React.FC = () => {
   return (
-    <div className="bg-white py-8 z-0">
-      <div className="container mx-auto">
+    <div className="bg-gradient-to-r from-blue-100 to-yellow-100 py-8 z-0">
+      <div className="px-auto">
         <Swiper
           spaceBetween={30}
           slidesPerView={1}
@@ -33,7 +32,7 @@ const PhotoGallery: React.FC = () => {
           }}
           breakpoints={{
             640: { slidesPerView: 2 },
-            768: { slidesPerView: 3 },
+            768: { slidesPerView: 2 },
           }}
           className="mySwiper"
         >
--- src/pages/AllLocationsDetails.tsx
+++ src/pages/AllLocationsDetails.tsx
@@ -2,7 +2,6 @@ import amore from "/officeimg/Amore/Amore.JPG"
 import makhija from "/officeimg/Makhija/Makhija.JPG"
 import ahmedabad from "/officeimg/Ahmedabad/ahmedabad.jpg"
 import matulya from "/officeimg/Matulya/Matulya.jpg";
-import { gallery6 } from '../utils/Landing/Landing';
 import sunshine from "/officeimg/Sunshine/sunshine.JPG"
 import Sunmill from "/officeimg/SunMill/Sunmill.jpeg"
 import { gallery5 } from '../utils/Landing/Landing';
@@ -13,6 +12,9 @@ type Location = {
   imgSrc: string;
   link: string;
   about : string
+  meetingroom : number,
+  conferenceroom : number,
+  daypass : number
 };
 
 export const locations: { city: string; locations: Location[] }[] = [
@@ -25,7 +27,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Bandra Kurla Complex, Kolivery Village, Mmrda Area, Bkc Cst Link Road, Bandra Kurla Complex- Bandra East, Mumbai - 400051",
           imgSrc: gallery5, // Replace with actual image link
           link: "/locations/Pinnacle-Corporate-Park",
-          about : "BKC offers a wide range of facilities to cater to the needs of its residents, employees, and visitors. The complex boasts of modern office spaces, commercial buildings, hotels, restaurants, and shopping centers. The presence of multinational corporations, financial institutions, and government offices in BKC has further enhanced its reputation as a prime business destination. Additionally, BKC is known for its green spaces and well-maintained infrastructure. Moreover, BKC is home to a mix of luxury hotels, upscale restaurants, and retail outlets, offering a perfect blend of work and leisure options."
+          about : "BKC offers a wide range of facilities to cater to the needs of its residents, employees, and visitors. The complex boasts of modern office spaces, commercial buildings, hotels, restaurants, and shopping centers. The presence of multinational corporations, financial institutions, and government offices in BKC has further enhanced its reputation as a prime business destination. Additionally, BKC is known for its green spaces and well-maintained infrastructure. Moreover, BKC is home to a mix of luxury hotels, upscale restaurants, and retail outlets, offering a perfect blend of work and leisure options.",
+          meetingroom : 899,
+          conferenceroom : 1499,
+          daypass : 999
         },
         {
           name: "Khar - Amore Centre",
@@ -33,7 +38,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Amore Centre, 5th Floor, Hasnabad Ln, Khar, Ram Krishna Nagar, Khar West, Mumbai, Maharashtra 400052",
           imgSrc: amore, // Replace with actual image link
           link: "/locations/Amore-Centre",
-          about : "603 The coworking space based in Khar west which is famous for being an entertainment and prime hub. Khar is one neighborhood that seamlessly brings together this city’s hustle bustle and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Khar and Bandra stations are at 5-7 minutes commute."
+          about : "603 The coworking space based in Khar west which is famous for being an entertainment and prime hub. Khar is one neighborhood that seamlessly brings together this city’s hustle bustle and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Khar and Bandra stations are at 5-7 minutes commute.",
+          meetingroom : 699,
+          conferenceroom : 1199,
+          daypass : 899
         },
         {
           name: "Bandra - Makhija Arcade",
@@ -41,7 +49,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Makhija Arcade, 6th Floor, 35th Rd, Khar, Khar West, Mumbai, Maharashtra 400052",
           imgSrc: makhija, // Replace with actual image link
           link: "/locations/Makhija-Archade",
-          about : "603 The coworking space based in Bandra west which is famous for being an entertainment and prime hub. Bandra is one neighbourhood that seamlessly brings together this city’s hustle bustle and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Khar and Bandra stations are at 5-7 minutes commute."
+          about : "603 The coworking space based in Bandra west which is famous for being an entertainment and prime hub. Bandra is one neighbourhood that seamlessly brings together this city’s hustle bustle and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Khar and Bandra stations are at 5-7 minutes commute.",
+          meetingroom : 699,
+          conferenceroom : 1199,
+          daypass : 899
         },
         {
           name: "Dadar - Sunshine Tower",
@@ -49,7 +60,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Sunshine Tower, 26th Floor, Plot No, 616, Tulsi Pipe Rd, Dadar West, Parel, Mumbai, Maharashtra 400013",
           imgSrc: sunshine, // Replace with actual image link
           link: "/locations/Sunshine-Tower",
-          about : "Sunshine Tower is based in Dadar which is famous for being an entertainment and commercial hub. Situated on the 29th Floor overlooking the most beautiful Mumbai Skyline. Dadar is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together."
+          about : "Sunshine Tower is based in Dadar which is famous for being an entertainment and commercial hub. Situated on the 29th Floor overlooking the most beautiful Mumbai Skyline. Dadar is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together.",
+          meetingroom : 699,
+          conferenceroom : 1199,
+          daypass : 899
         },
         {
           name: "Lower Parel - Matulya Centre",
@@ -57,15 +71,21 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Matulya Centre, 2nd Floor, Lower Parel, Mumbai, Maharashtra 400013",
           imgSrc: matulya, // Replace with actual image link
           link: "/locations/Matulya-Centre",
-          about : "Matulya Centre is based in Lower Parel, famous for being an entertainment and commercial hub. Lower Parel seamlessly brings together this city’s commercial and entertainment aspects. Our centre is situated on the main road, making it easy to travel by rail and bus. Lower Parel and Currey Road station is just a 5-minute walk away."
+          about : "Matulya Centre is based in Lower Parel, famous for being an entertainment and commercial hub. Lower Parel seamlessly brings together this city’s commercial and entertainment aspects. Our centre is situated on the main road, making it easy to travel by rail and bus. Lower Parel and Currey Road station is just a 5-minute walk away.",
+          meetingroom : 599,
+          conferenceroom : 1099,
+          daypass : 799
         },
         {
           name: "Lower Parel - Kamala Mills",
           address:
             "Kamala Mills, 4th Floor, Lower Parel, Mumbai, Maharashtra 400013",
-          imgSrc: gallery6, // Replace with actual image link
+          imgSrc: '/officeimg/Kamala/kamala7.jpg', // Replace with actual image link
           link: "/locations/Kamala-Mills",
-          about : "Our new location at Kamala Mills Compound is based in Lower Parel which is famous for being an entertainment and commercial hub. Lower Parel is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together. Our centre is situated in Kamala Mills Compound one of the most Prime Commercial location in Lower Parel with easy connectivity to rail and bus. Lower Parel and Currey Road station is at 10 minutes walking distance."
+          about : "Our new location at Kamala Mills Compound is based in Lower Parel which is famous for being an entertainment and commercial hub. Lower Parel is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together. Our centre is situated in Kamala Mills Compound one of the most Prime Commercial location in Lower Parel with easy connectivity to rail and bus. Lower Parel and Currey Road station is at 10 minutes walking distance.",
+          meetingroom : 799,
+          conferenceroom : 1499,
+          daypass : 899
         },
         {
           name: "Lower Parel - Sun Mill Compound",
@@ -73,7 +93,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "Sun Mill Compound, Unit 15, Cama Industrial Estate, Mumbai 400013",
           imgSrc: Sunmill, // Replace with actual image link
           link: "/locations/Sun-Mill-Compound",
-          about : "Sun Mill Compound is based in Lower Parel which is famous for being an entertainment and commercial hub. Lower Parel is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Lower Parel and Currey Road station is at 5 minutes walking distance. If you are sociable, you can find several restaurants, food joints and gaming zone in the proximity. Palladium mall is at 5 minutes distance which has a multiplex, 5-star restaurants, four levels of exclusive shopping area."
+          about : "Sun Mill Compound is based in Lower Parel which is famous for being an entertainment and commercial hub. Lower Parel is one neighbourhood that seamlessly brings together this city’s commercial and entertainment aspects together. Our centre is situated on the main-road which helps the commuter to travel easily by rail and bus. Lower Parel and Currey Road station is at 5 minutes walking distance. If you are sociable, you can find several restaurants, food joints and gaming zone in the proximity. Palladium mall is at 5 minutes distance which has a multiplex, 5-star restaurants, four levels of exclusive shopping area.",
+          meetingroom : 499,
+          conferenceroom : 999,
+          daypass : 599
         },
         {
           name: "Pentagon Classic",
@@ -81,7 +104,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "11, Western Express Hwy, Parshiwada, Chakala, Andheri East, Mumbai, Maharashtra 400053",
           imgSrc: '/officeimg/Pentagon/pentagon.JPG', // Replace with actual image link
           link: "/locations/Pentagon-Classic",
-          about : "Located in the heart of Navi Mumbai, our Technocity center offers a modern, dynamic workspace designed for businesses of all sizes. Strategically positioned near major corporate hubs and with excellent connectivity to highways, this space is perfect for professionals seeking convenience and productivity."
+          about : "Located in the heart of Navi Mumbai, our Technocity center offers a modern, dynamic workspace designed for businesses of all sizes. Strategically positioned near major corporate hubs and with excellent connectivity to highways, this space is perfect for professionals seeking convenience and productivity.",
+          meetingroom : 399,
+          conferenceroom : 699,
+          daypass : 599
         },
       ],
     },
@@ -92,17 +118,23 @@ export const locations: { city: string; locations: Location[] }[] = [
           name: "Millennium Business Park",
           address:
             "Millennium Business Park, Sector 2, Kopar Khairane, Navi Mumbai, Maharashtra 400701",
-          imgSrc: "/officeimg/Rupa/Rupa2.jpg", // Replace with actual image link
+          imgSrc: "/officeimg/Rupa/Rupa4.jpg", // Replace with actual image link
           link: "/locations/Millenium-Business-Park",
-          about : "Discover the epitome of flexibility with our coworking office spaces in Ghansoli. Our brand-new facility is designed to revolutionize your work experience, providing state-of-the-art coworking office spaces in Ghansoli that seamlessly blend innovation, collaboration, and comfort. Our ergonomic furniture, high-speed internet, and cutting-edge technology ensure that your workday is seamless and efficient. At 603, we understand the evolving needs of professionals and businesses, and our Ghansoli location is tailored to meet those demands."
+          about : "Discover the epitome of flexibility with our coworking office spaces in Ghansoli. Our brand-new facility is designed to revolutionize your work experience, providing state-of-the-art coworking office spaces in Ghansoli that seamlessly blend innovation, collaboration, and comfort. Our ergonomic furniture, high-speed internet, and cutting-edge technology ensure that your workday is seamless and efficient. At 603, we understand the evolving needs of professionals and businesses, and our Ghansoli location is tailored to meet those demands.",
+          conferenceroom : 799,
+          daypass : 499,
+          meetingroom : 399
         },
         {
           name: "Technocity",
           address:
             "Technocity, 5th Floor, Navi Mumbai, Maharashtra 400701",
-          imgSrc: "officeimg/Technocity/Techno.JPG", // Replace with actual image link
+          imgSrc: '/officeimg/Technocity/Techno2.JPG', // Replace with actual image link
           link: "/locations/Technocity",
-          about : "Come experience the perfect balance of work and community at 603 The Coworking Space, Technocity, Navi Mumbai."
+          about : "Come experience the perfect balance of work and community at 603 The Coworking Space, Technocity, Navi Mumbai.",
+          conferenceroom : 799,
+          daypass : 499,
+          meetingroom : 399
         },
       ],
     },
@@ -115,7 +147,10 @@ export const locations: { city: string; locations: Location[] }[] = [
             "B306/ 307/ 308 - Navratna Corporate Park, Ashok Vatika, Ambli Bopal Road, Ahmedabad, Gujarat 380058",
           imgSrc: ahmedabad, // Replace with actual image link
           link: "/locations/Navratna-Corporate-Park",
-          about : "603 The Coworking Space is excited to announce the opening of our newest office space in the vibrant city of Ahmedabad, strategically located at Navratna Corporate Park. This state-of-the-art facility offers a dynamic and innovative environment designed to foster productivity and collaboration. Our Ahmedabad office space is equipped with modern amenities and cutting-edge technology, ensuring a seamless working experience for all our members. Navratna Corporate Park is one of Ahmedabad's most prestigious business hubs Situated in the heart of the city, the office space is easily accessible from major residential and commercial areas."
+          about : "603 The Coworking Space is excited to announce the opening of our newest office space in the vibrant city of Ahmedabad, strategically located at Navratna Corporate Park. This state-of-the-art facility offers a dynamic and innovative environment designed to foster productivity and collaboration. Our Ahmedabad office space is equipped with modern amenities and cutting-edge technology, ensuring a seamless working experience for all our members. Navratna Corporate Park is one of Ahmedabad's most prestigious business hubs Situated in the heart of the city, the office space is easily accessible from major residential and commercial areas.",
+          conferenceroom : 799,
+          daypass : 599,
+          meetingroom : 399 
         },
       ],
     }
