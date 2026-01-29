4d1caef6dcffefde95dcb65fe11dbf6700aab7c1
Yuvraj <yuvrajmanchadi321@gmail.com>
2026-01-29 18:53:49 +0530

    added the new testimonal and adjusted the height for the card

--- src/components/Landing/Review.tsx
+++ src/components/Landing/Review.tsx
@@ -3,32 +3,61 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "./../../../node_modules/swiper/swiper-bundle.min.css";
 import SwiperCore from "swiper";
 import { Autoplay } from "swiper/modules";
-import { logo } from "../../utils/Landing/Landing";
+// import { logo } from "../../utils/Landing/Landing";
 import { useEffect, useState } from "react";
 
 // Install Swiper modules
 SwiperCore.use([Autoplay]);
 
-const reviews = [
+// const reviews = [
+//   {
+//     text: "603 The Coworking Space has been a game-changer for my freelancing career. The atmosphere is incredibly inspiring, and the amenities they offer are top-notch. From high-speed internet to well-equipped meeting rooms, it's a professional's dream. The community here is so supportive, and I've made valuable connections that have led to new opportunities. Highly recommend!",
+//     author: "Vihang",
+//   },
+//   {
+//     text: "As a startup founder, finding the right environment to grow my business was crucial. 603 The Coworking Space exceeded my expectations. The flexibility of their plans allowed me to scale up as my team grew, and the office infrastructure is designed with startups in mind. The events they organize are both educational and fun, fostering networking and learning. It's more than just a workspace; it's a launchpad for success.",
+//     author: "Siddharth",
+//   },
+//   {
+//     text: "Remote work can sometimes feel isolating, but thanks to 603 The Coworking Space, I've regained that sense of community. The open layout encourages collaboration, and the diverse range of professionals here sparks fresh ideas. The cozy lounges and well-stocked kitchenette make breaks enjoyable, and l've found it easier to strike a work-life balance. Joining this space was a fantastic decision for my productivity and well-being",
+//     author: "Maira",
+//   },
+//   {
+//     text: "603 The Coworking Space embodies professionalism and convenience. As a consultant, I often meet clients here, and the modern meeting rooms are impressive. The staff is attentive and always ready to assist with any needs. Plus, the location is central, making it easy for my clients to access. If you're looking for a polished and productive workspace, this is the place to be.",
+//     author: "Rajiv",
+//   },
+//   {
+//     text: "Creativity flows freely at 603 The Coworking Space. As a content creator, I find the ambiance truly inspiring. The natural light, comfortable workstations, and collaborative vibe have boosted my productivity. Being surrounded by fellow creatives has led to unexpected collaborations and projects. It's like working in a hub of innovation, and I couldn't be happier with my choice.",
+//     author: "Tushar",
+//   },
+// ];
+
+const clientTestimonials = [
   {
-    text: "603 The Coworking Space has been a game-changer for my freelancing career. The atmosphere is incredibly inspiring, and the amenities they offer are top-notch. From high-speed internet to well-equipped meeting rooms, it's a professional's dream. The community here is so supportive, and I've made valuable connections that have led to new opportunities. Highly recommend!",
-    author: "Vihang",
+    company: "Finman Capital Service",
+    author: "Mr. Bhavesh Sheth",
+    designation: "",
+    logo: "/ClientLogos/finmanCapital.jpeg",
+    text: `We are delighted to be a part of the 603 family. Our company has a legacy of over 36 years, and being here has given us the flexibility to scale effortlessly, thanks to the ample space and well-planned infrastructure. The availability of multiple meeting rooms, lunch facilities, and excellent staff amenities adds immense value to our day-to-day operations. All these benefits come at a very attractive and cost-effective proposition.
+Being associated with 603 has truly been fortunate for us. During this journey, we were honored with three national awards. We received recognition from The Economic Times as the largest player in real estate and logistics, from Zee Business as the largest player in real estate and the fastest-growing financial services and insurance company of the year, and from Edelweiss Securities as one of the leading insurance players in India.`,
   },
   {
-    text: "As a startup founder, finding the right environment to grow my business was crucial. 603 The Coworking Space exceeded my expectations. The flexibility of their plans allowed me to scale up as my team grew, and the office infrastructure is designed with startups in mind. The events they organize are both educational and fun, fostering networking and learning. It's more than just a workspace; it's a launchpad for success.",
-    author: "Siddharth",
+    company: "Whoppls",
+    author: "Ramya Ramachandra",
+    designation: "Founder",
+    logo: "/ClientLogos/whoppl.jpeg",
+    text: `I’m Ramya Ramachandra, Founder of Whoppls. We are a content-to-commerce company and have been in business for four and a half years. As a company that started during the pandemic, transitioning back to an office environment was a significant step for us, and 603 proved to be the most viable choice.
+Working with Kunal has been a wonderful experience—he is not only a brilliant entrepreneur but also a genuinely great human being. He gave us complete freedom to design our office space in line with our vision.
+We began with one office and have now expanded to another, which speaks volumes about our journey with 603. The experience has been seamless, from team bonding activities to resources and day-to-day operations. It gives me immense comfort as an entrepreneur knowing that all the essential details are in good hands, allowing us to focus entirely on growth.`,
   },
   {
-    text: "Remote work can sometimes feel isolating, but thanks to 603 The Coworking Space, I've regained that sense of community. The open layout encourages collaboration, and the diverse range of professionals here sparks fresh ideas. The cozy lounges and well-stocked kitchenette make breaks enjoyable, and l've found it easier to strike a work-life balance. Joining this space was a fantastic decision for my productivity and well-being",
-    author: "Maira",
-  },
-  {
-    text: "603 The Coworking Space embodies professionalism and convenience. As a consultant, I often meet clients here, and the modern meeting rooms are impressive. The staff is attentive and always ready to assist with any needs. Plus, the location is central, making it easy for my clients to access. If you're looking for a polished and productive workspace, this is the place to be.",
-    author: "Rajiv",
-  },
-  {
-    text: "Creativity flows freely at 603 The Coworking Space. As a content creator, I find the ambiance truly inspiring. The natural light, comfortable workstations, and collaborative vibe have boosted my productivity. Being surrounded by fellow creatives has led to unexpected collaborations and projects. It's like working in a hub of innovation, and I couldn't be happier with my choice.",
-    author: "Tushar",
+    company: "SEED Global",
+    author: "Mr. Sharang Dhaimade",
+    designation: "",
+    logo: "/ClientLogos/seed.jpeg",
+    text: `We have been a part of 603 Co-Working Space for nearly a year, and the experience has been excellent.
+The strong community and welcoming people were key reasons we chose 603. Its proximity to Lower Parel, Mumbai’s central business district, made it an ideal location.
+The pricing is fair, the team is supportive, and the facilities are well maintained. Most importantly, the workspace offers privacy and a focused environment, allowing us to work efficiently.`,
   },
 ];
 
@@ -47,7 +76,7 @@ const Review: React.FC = () => {
   }, []);
 
   return (
-    <div className="bg-gradient-to-r from-white to-yellow-100 py-12">
+    <div className="bg-gradient-to-r from-white to-yellow-100 md:py-12">
       <div className="container mx-auto text-center px-5 md:px-10">
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-700 mb-12">
           Our Valued Clients...
@@ -67,23 +96,28 @@ const Review: React.FC = () => {
           }}
           className="mySwiper"
         >
-          {reviews.map((review, index) => (
+          {clientTestimonials?.map((review, index) => (
             <SwiperSlide key={index}>
               <div
-                className="bg-white py-6 rounded-lg shadow-lg flex flex-col items-center"
+                className="bg-white my-4 py-6 rounded-lg shadow-lg flex flex-col items-center"
                 style={{
                   paddingLeft: windowWidth > 1277 ? "20px" : "15px",
                   paddingRight: windowWidth > 1277 ? "20px" : "15px",
-                  height: windowWidth > 390 ? "400px" : "420px",
+                  height: windowWidth > 390 ? "550px" : "650px",
                   boxShadow:
                     "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
                 }}
               >
                 <div className="mb-4">
-                  <img src={logo} alt="Company Logo" className="h-14 mb-4" />
+                  <img
+                    src={review?.logo}
+                    alt="Company Logo"
+                    className="h-14 w-18 mb-4"
+                  />
+                  {/* <img src={logo} alt="Company Logo" className="h-14 mb-4" /> */}
                 </div>
 
-                <p className="text-gray-600 mb-8 text-sm leading-relaxed w-full max-w-xl mx-auto text-justify">
+                <p className="text-gray-600 mb-8 text-xs sm:text-sm leading-relaxed w-full max-w-xl mx-auto text-justify">
                   {review.text}
                 </p>
 
