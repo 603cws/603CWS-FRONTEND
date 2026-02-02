6e1c6e494794fd19c49265b003f982b595e99c10
SunnyBandre <bandresunny89@gmail.com>
2026-02-02 15:49:53 +0530

    Added Effect for Whatsapp btn

--- src/utils/WhatsappBtn.tsx
+++ src/utils/WhatsappBtn.tsx
@@ -11,7 +11,7 @@ function WhatsappBtn() {
       href={whatsappURL}
       target="_blank"
       rel="noopener noreferrer"
-      className="z-50 fixed bottom-5 right-5 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600"
+      className="z-50 fixed bottom-5 right-5 bg-green-500 text-white p-4 rounded-full scale-75 hover:scale-100 transition duration-300 ease-in-out shadow-lg hover:bg-green-600"
     >
       <FaWhatsapp size={28} />
     </a>
